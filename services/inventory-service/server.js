f097be79558e1685d9cf0e3760cbd3e13d7683de
Dheeraj Goswami <132127084+dheeujxs@users.noreply.github.com>
2026-08-23 15:59:11 +0530

    Update server.js

--- services/inventory-service/server.js
+++ services/inventory-service/server.js
@@ -67,7 +67,7 @@ connectDB();
 // ============================================
 
 const allowedOrigins = [
-   "https://restautant-pos-7ruu.vercel.app",
+   "https://restautant-pos-lime.vercel.app",
   'http://localhost:5173',
   'http://localhost:3000',
   'http://127.0.0.1:5173',
